749357edb9141bc16fb09cbbcb75690824df0201
Якимов Артем Дмитриевич <yakimov_a_qschool@qsoft.ru>
2024-05-29 15:08:42 +0300

    Страница определенной вещи 0.2

--- src/app/gallery/[item]/page.tsx
+++ src/app/gallery/[item]/page.tsx
@@ -2,12 +2,21 @@ import Image from "next/image";
 import Link from "next/link";
 import arrowIcon from "../../../assets/icons/arrow-swap-right.svg";
 
+export async function generateStaticParams() {
+    return [
+        { item: "1" },
+        { item: "2" },
+        { item: "3" },
+        { item: "4" },
+        { item: "5" },
+        { item: "6" },
+    ];
+}
 
-
-export default async function Page({ params }: { params: { item: string } }) {
+export default async function Page({ params }: { params: { item: number } }) {
     const data = await Promise.all([
-        require(`../../../assets/img/main/main${params.item}.jpg`),
-        require(`../../../assets/img/main/main${params.item}.jpg`),
+        require(`../../../assets/img/main/main${params.item % 3 + 1}.jpg`),
+        require(`../../../assets/img/main/main${params.item % 3 + 1}.jpg`),
         require(`../../../assets/img/main/main2.jpg`),
     ]);
 
